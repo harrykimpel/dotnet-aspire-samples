8f2ccf3cf51337a048c8622e8873da48c91cbd23
Harry Kimpel <harrykimpel@hotmail.com>
2024-10-14 16:51:28 +0200

    Minor updates
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- samples/AspireWithDapr/AspireWithDapr.ApiService/Program.cs
+++ samples/AspireWithDapr/AspireWithDapr.ApiService/Program.cs
@@ -21,34 +21,44 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    app.Logger.LogInformation("Receiving request for weather forecast.");
-    int maxRange = 5;
-    var forecast = Enumerable.Range(1, maxRange).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-
-    var activity = Activity.Current;
-    activity?.SetTag("amountWeatherForecasts", forecast.Length);
-
-    int totalTempC = 0;
-    int totalTempF = 0;
-    foreach (WeatherForecast fc in forecast)
+    try
     {
-        totalTempC += fc.TemperatureC;
-        totalTempF += fc.TemperatureF;
-    }
-    float avgTempC = (float)totalTempC / (float)maxRange;
-    float avgTempF = (float)totalTempF / (float)maxRange;
-    activity?.SetTag("avgTemperatureC", avgTempC);
-    activity?.SetTag("avgTemperatureF", avgTempF);
-    app.Logger.LogInformation($"The average temperature in the next {maxRange} days is {avgTempC} C or {avgTempF} F.");
+        app.Logger.LogInformation("Receiving request for weather forecast.");
+        int maxRange = 5;
+        var forecast = Enumerable.Range(1, maxRange).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
 
-    return forecast;
+        var activity = Activity.Current;
+        activity?.SetTag("amountWeatherForecasts", forecast.Length);
+
+        int totalTempC = 0;
+        int totalTempF = 0;
+        foreach (WeatherForecast fc in forecast)
+        {
+            totalTempC += fc.TemperatureC;
+            totalTempF += fc.TemperatureF;
+        }
+        float avgTempC = (float)totalTempC / (float)maxRange;
+        float avgTempF = (float)totalTempF / (float)maxRange;
+        activity?.SetTag("avgTemperatureC", avgTempC);
+        activity?.SetTag("avgTemperatureF", avgTempF);
+        app.Logger.LogInformation($"The average temperature in the next {maxRange} days is {avgTempC} C or {avgTempF} F.");
+
+        return Results.Ok(forecast);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception: {ex}");
+        app.Logger.LogInformation($"Exception: {ex}");
+    }
+
+    return Results.Ok("");
 });
 
 //string baseURL = (Environment.GetEnvironmentVariable("BASE_URL") ?? "http://localhost") + ":" + (Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500"); //reconfigure cpde to make requests to Dapr sidecar
@@ -59,7 +69,7 @@ const string TOPIC = "orders";
 app.MapGet("/dapr/subscribe", () =>
 {
     var sub = new DaprSubscription(PubsubName: PUBSUBNAME, Topic: TOPIC, Route: TOPIC);
-    Console.WriteLine("Dapr pub/sub is subscribed to: " + sub);
+    Console.WriteLine($"Dapr pub/sub is subscribed to: {sub}");
     return Results.Json(new DaprSubscription[] { sub });
 });
 
@@ -67,16 +77,16 @@ app.MapGet("/dapr/subscribe", () =>
 app.MapPost("/orders", (DaprData<Order> requestData) =>
 {
     int orderId = requestData.Data.OrderId;
-    Console.WriteLine("Subscriber received Order Id: " + orderId);
+    Console.WriteLine($"Subscriber received Order Id: {orderId}");
     app.Logger.LogInformation($"Subscriber received Order Id: {orderId}");
     var activity = Activity.Current;
     activity?.SetTag("orderId", orderId);
 
     if (orderId % 10 == 0)
     {
-        // Simulate an error for every 10th order
+        // Simulate an error for roughly every 10th order
         activity?.SetStatus(ActivityStatusCode.Error, "Something bad happened!");
-        throw new Exception("Simulated error for order id: " + orderId);
+        throw new Exception($"Simulated error for order id: {orderId}");
     }
 
     if (20 <= orderId && orderId <= 40)
