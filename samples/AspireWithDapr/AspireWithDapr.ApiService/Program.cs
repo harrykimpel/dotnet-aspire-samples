6fc9ab266e09a103e13768c549bb08e8c6286b23
Harry Kimpel <harrykimpel@hotmail.com>
2024-10-15 13:15:46 +0200

    Adding dead letter queue for pub-sub use case
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- samples/AspireWithDapr/AspireWithDapr.ApiService/Program.cs
+++ samples/AspireWithDapr/AspireWithDapr.ApiService/Program.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -61,19 +63,19 @@ app.MapGet("/weatherforecast", () =>
     return Results.Ok("");
 });
 
+// Register Dapr pub/sub subscriptions programmatically
 //string baseURL = (Environment.GetEnvironmentVariable("BASE_URL") ?? "http://localhost") + ":" + (Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500"); //reconfigure cpde to make requests to Dapr sidecar
-const string PUBSUBNAME = "orderpubsub";
-const string TOPIC = "orders";
+//const string PUBSUBNAME = "orderpubsub";
+//const string TOPIC = "orders";
 
-// Register Dapr pub/sub subscriptions
-app.MapGet("/dapr/subscribe", () =>
+/*app.MapGet("/dapr/subscribe", () =>
 {
     var sub = new DaprSubscription(PubsubName: PUBSUBNAME, Topic: TOPIC, Route: TOPIC);
     Console.WriteLine($"Dapr pub/sub is subscribed to: {sub}");
     return Results.Json(new DaprSubscription[] { sub });
-});
+});*/
 
-// Dapr subscription in /dapr/subscribe sets up this route
+// Dapr subscription in /dapr/subscribe sets up this route when configuring it programmatically
 app.MapPost("/orders", (DaprData<Order> requestData) =>
 {
     int orderId = requestData.Data.OrderId;
@@ -85,6 +87,7 @@ app.MapPost("/orders", (DaprData<Order> requestData) =>
     if (orderId % 10 == 0)
     {
         // Simulate an error for roughly every 10th order
+        app.Logger.LogInformation($"Simulated error for order id: {orderId}");
         activity?.SetStatus(ActivityStatusCode.Error, "Something bad happened!");
         throw new Exception($"Simulated error for order id: {orderId}");
     }
@@ -100,6 +103,49 @@ app.MapPost("/orders", (DaprData<Order> requestData) =>
     return Results.Ok(requestData.Data);
 });
 
+app.MapPost("/failedOrders", async (DaprData<Order> requestData) =>
+{
+    try
+    {
+        int orderId = requestData.Data.OrderId;
+        Console.WriteLine($"Subscriber received failed Order Id: {orderId}");
+        app.Logger.LogInformation($"Subscriber received failed Order Id: {orderId}");
+        var activity = Activity.Current;
+        activity?.SetTag("orderId", orderId);
+
+        // send to New Relic Insights event
+        FailedOrder failedOrder = new FailedOrder(eventType: "FailedOrder", OrderId: orderId);
+        HttpClient httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        var NEW_RELIC_ACCOUNT_ID = Environment.GetEnvironmentVariable("NEW_RELIC_ACCOUNT_ID");
+        var NEW_RELIC_INSIGHTS_INSERT_KEY = Environment.GetEnvironmentVariable("NEW_RELIC_INSIGHTS_INSERT_KEY");
+        httpClient.DefaultRequestHeaders.Add("X-Insert-Key", NEW_RELIC_INSIGHTS_INSERT_KEY);
+        string url2 = $"https://insights.newrelic.com/v1/accounts/{NEW_RELIC_ACCOUNT_ID}/events";
+        var url = $"https://insights-collector.newrelic.com/v1/accounts/{NEW_RELIC_ACCOUNT_ID}/events";
+
+        var response = await httpClient.PostAsync(url, new StringContent(JsonSerializer.Serialize<FailedOrder>(failedOrder), Encoding.UTF8, "application/json"));
+
+        string result = response.Content.ReadAsStringAsync().Result;
+
+        //app.Logger.LogInformation("NR result: " + result);
+
+        if (response.IsSuccessStatusCode)
+        {
+            app.Logger.LogInformation($"Failed order received (order id: {orderId}) and sent to custom New Relic event for investigation.");
+        }
+        else
+        {
+            app.Logger.LogInformation($"Failed to send failed order id: {orderId} to New Relic event.");
+            app.Logger.LogInformation($"Response: {response}");
+        }
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogInformation($"Exception: {ex}");
+    }
+    return Results.Ok(requestData.Data);
+});
+
 app.MapDefaultEndpoints();
 
 app.Run();
@@ -111,6 +157,7 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 
 public record DaprData<T>([property: JsonPropertyName("data")] T Data);
 public record Order([property: JsonPropertyName("orderId")] int OrderId);
+public record FailedOrder([property: JsonPropertyName("eventType")] string eventType, [property: JsonPropertyName("orderId")] int OrderId);
 public record DaprSubscription(
   [property: JsonPropertyName("pubsubname")] string PubsubName,
   [property: JsonPropertyName("topic")] string Topic,
